a1cea5a0fd6a96a86e3fb384c973e8056738c369
Luiz Fernando <luiz.fpimentel0@gmail.com>
2024-11-07 23:19:38 -0300

    feat: default components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Header } from "./components/Header/";
+import { Pokedex } from "./components/Pokedex/";
+import { Home } from "./components/Home/";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokedex" element={<Pokedex />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
